1c49db0cb771f606072510061a5a67b6980919a1
Matthew Emes <memes@matthewemes.com>
2025-03-10 17:01:14 -0700

    test(golangci-lint): Alpine repo has 1.57.2
    The standard Alpine repo linked to the Alpine image used here has
    golangci-lint v1.57.2-r2; changed the test script and scenario to match.

--- test/golangci-lint/alpine_version.sh
+++ test/golangci-lint/alpine_version.sh
@@ -4,6 +4,6 @@ set -e
 # shellcheck disable=SC1091
 . alpine-features-test-lib.sh
 
-check "Verify golangci-lint is v1.64.5" sh -c "golangci-lint --version | grep '1\.64\.5'"
+check "Verify golangci-lint is v1.57.2" sh -c "golangci-lint --version | grep '1\.57\.2'"
 
 reportResults
