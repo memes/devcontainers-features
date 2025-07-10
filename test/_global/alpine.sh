c9d8d69cf07783921b8413a57bc344ff11dab3be
Matthew Emes <memes@matthewemes.com>
2025-07-09 17:43:39 -0700

    test: Consolidate _global tests where possible
    With the exception of UBI, which doesn't have sudo and has conflicting
    base packages, essentially the same test commands are used for all the
    other distros. This pulls those into common.sh to minimize the chance of
    missing a test case when updating repo.

--- test/_global/alpine.sh
+++ test/_global/alpine.sh
@@ -4,25 +4,5 @@ set -e
 # shellcheck disable=SC1091
 . alpine-features-test-lib.sh
 
-check "Verify a version of direnv is installed" direnv version
-check "Verify a version of gcloud is installed" sh -c "gcloud version | grep 'Google Cloud SDK'"
-check "Verify a version of tofu is installed" tofu version
-check "Verify a version of starship is installed" starship --version
-check "Verify a version of talisman is installed" talisman --version
-check "Verify a version of terraform-docs is installed" terraform-docs --version
-check "Verify a version of terragrunt is installed" terragrunt --version
-check "Verify a version of tflint is installed" tflint --version
-check "Verify a version of packer is installed" packer -version
-check "Verify a version of terraform is installed" terraform -version
-[ -f /usr/local/bin/vault ] && sudo setcap cap_ipc_lock=-ep /usr/local/bin/vault
-check "Verify a version of vault is installed" vault -version
-check "Verify a version of buf is installed" buf --version
-check "Verify a version of goreleaser is installed" goreleaser --version
-check "Verify a version of golangci-lint is installed" golangci-lint --version
-check "Verify a version of sops is installed" sops --version
-check "Verify a version of age is installed " age --version
-check "Verify a version of GnuPG is installed " gpg --version
-check "Verify a version of vesctl is installed " vesctl version
-check "Verify a version of flux is installed " flux version --client
-check "Verify a version of hadolint is installed " hadolint --version
-reportResults
+# shellcheck disable=SC1091
+. common.sh
--- test/_global/common.sh
+++ test/_global/common.sh
@@ -1,10 +1,8 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 #
-# UBI images are special and cannot use common.sh without customization and branching.
-set -e
+# Validation common to all global scenarios except UBI.
 
-# shellcheck disable=SC1091
-source dev-container-features-test-lib
+set -e
 
 check "Verify a version of direnv is installed" direnv version
 check "Verify a version of gcloud is installed" sh -c "gcloud version | grep 'Google Cloud SDK'"
@@ -14,17 +12,20 @@ check "Verify a version of talisman is installed" talisman --version
 check "Verify a version of terraform-docs is installed" terraform-docs --version
 check "Verify a version of terragrunt is installed" terragrunt --version
 check "Verify a version of tflint is installed" tflint --version
-check "Verify a version of packer is installed" /usr/bin/packer -version
+check "Verify a version of packer is installed" packer -version
 check "Verify a version of terraform is installed" terraform -version
-[ -f /usr/bin/vault ] && setcap cap_ipc_lock=-ep /usr/bin/vault
+[ -f /usr/bin/vault ] && sudo setcap cap_ipc_lock=-ep /usr/bin/vault
+[ -f /usr/local/bin/vault ] && sudo setcap cap_ipc_lock=-ep /usr/local/bin/vault
 check "Verify a version of vault is installed" vault -version
 check "Verify a version of buf is installed" buf --version
 check "Verify a version of goreleaser is installed" goreleaser --version
 check "Verify a version of golangci-lint is installed" golangci-lint --version
 check "Verify a version of sops is installed" sops --version
-check "Verify a version of age is NOT installed" sh -c '! age --version 2>/dev/null'
+check "Verify a version of age is installed " age --version
 check "Verify a version of GnuPG is installed " gpg --version
 check "Verify a version of vesctl is installed " vesctl version
 check "Verify a version of flux is installed " flux version --client
 check "Verify a version of hadolint is installed " hadolint --version
+check "Verify a version of gcrane is installed " gcrane version
+
 reportResults
