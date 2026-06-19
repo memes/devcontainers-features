f6cd12f259ee7ff6ab385eea440e1d123567301b
Matthew Emes <memes@matthewemes.com>
2026-06-19 19:15:58 +0000

    chore: Drop terraform-docs feature

--- test/_global/common.sh
+++ test/_global/common.sh
@@ -6,7 +6,6 @@ set -e
 
 check "Verify a version of gcloud is installed" sh -c "gcloud version | grep 'Google Cloud SDK'"
 check "Verify a version of talisman is installed" talisman --version
-check "Verify a version of terraform-docs is installed" terraform-docs --version
 check "Verify a version of terragrunt is installed" terragrunt --version
 check "Verify a version of tflint is installed" tflint --version
 check "Verify a version of packer is installed" packer -version
--- test/_global/ubi.sh
+++ test/_global/ubi.sh
@@ -8,7 +8,6 @@ source dev-container-features-test-lib
 
 check "Verify a version of gcloud is installed" sh -c "gcloud version | grep 'Google Cloud SDK'"
 check "Verify a version of talisman is installed" talisman --version
-check "Verify a version of terraform-docs is installed" terraform-docs --version
 check "Verify a version of terragrunt is installed" terragrunt --version
 check "Verify a version of tflint is installed" tflint --version
 check "Verify a version of packer is installed" /usr/bin/packer -version
