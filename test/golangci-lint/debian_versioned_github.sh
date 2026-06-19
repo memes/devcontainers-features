3ab93be533923dc999177dc2eab7fcb766736de7
Matthew Emes <memes@matthewemes.com>
2026-06-19 19:04:00 +0000

    chore: Drop golangci-lint from repo

--- test/golangci-lint/debian_versioned_github.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# shellcheck disable=SC1091
-source dev-container-features-test-lib
-
-check "Verify golangci-lint is v1.64.5" sh -c "golangci-lint --version | grep '1\.64\.5'"
-
-reportResults
