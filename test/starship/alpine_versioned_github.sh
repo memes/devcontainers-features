9ac66061ecdeaa5110415882d3922344a46a7eed
Matthew Emes <memes@matthewemes.com>
2026-06-19 19:12:51 +0000

    chore: Drop starship feature

--- test/starship/alpine_versioned_github.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-set -e
-
-# shellcheck disable=SC1091
-. alpine-features-test-lib.sh
-
-check "Starship version is v1.22.0" sh -c "starship --version | grep '1.22.0'"
-
-reportResults
--- test/starship/debian_version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# shellcheck disable=SC1091
-source dev-container-features-test-lib
-
-check "Starship version is v1.22.0" sh -c "starship --version | grep '1.22.0'"
-
-reportResults
